76a5aaf4264f47051b5989ac9681f6b75c116849
qpounodb <qpundb+github@ya.ru>
2022-08-18 20:44:39 +0300

    refactor: classname usage

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -15,16 +15,14 @@ export type InputProps = Omit<
 export const Input: React.FC<InputProps> = ({
   value,
   onChange,
-  className,
+  className = '',
   ...args
 }) => {
-  const cls = classname(
-    'input',
-    {
-      input_disabled: args.disabled,
-    },
-    className
-  );
+  const cls = classname({
+    input: true,
+    input_disabled: args.disabled,
+    [className]: className.length > 0,
+  });
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value);
